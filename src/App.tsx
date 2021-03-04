25cd9cb1ff3f48ce33c2ca6cebd8a00b6ad4fad5
everton-lacerda <everton_ml@outlook.com>
2021-03-04 19:47:34 -0300

    create grid project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from './components/Layout'; 
+
+import GlobalStyles from "./styles/GlobalStyles";
+
+const App: React.FC = () => {
+    return (
+        <>
+            <GlobalStyles />
+            <Layout />
+        </>
+    );
+}
+
+export default App;
