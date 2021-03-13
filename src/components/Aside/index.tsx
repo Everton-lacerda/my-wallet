2396a53b1a96db7141a5994dab6b9881355413b0
everton-lacerda <everton_ml@outlook.com>
2021-03-13 16:54:20 -0300

    feat: add menu, icons, styled

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
 
-import { Container } from './style'
+import { Container, Header, LogoImg, MenuContainer, MenuItemLink, Title } from './style'
+
+import logoimg from '../../assets/logo.svg'
+
+import { 
+   MdDashboard,
+   MdArrowDownward,
+   MdArrowUpward,
+   MdExitToApp 
+    
+} from 'react-icons/md';
+
 
 const Aside: React.FC = () => {
     return (
         <Container >
-            <h1>Aside</h1>
+            <Header>
+                <LogoImg src={logoimg} alt="logo minha wallet" />
+                <Title>Minha Carteira</Title>
+            </Header>
+
+            <MenuContainer>
+                <MenuItemLink href="#" >
+                    <MdDashboard />
+                    Dashboard
+                </MenuItemLink>
+                <MenuItemLink href="#" >
+                    <MdArrowDownward />
+                    Entradas
+                </MenuItemLink>
+                <MenuItemLink href="#" >
+                    <MdArrowUpward />
+                    Saidas
+                </MenuItemLink>
+                <MenuItemLink href="#" >
+                    <MdExitToApp />
+                    Sair
+                </MenuItemLink>
+            </MenuContainer>
         </Container>
     );
 }
