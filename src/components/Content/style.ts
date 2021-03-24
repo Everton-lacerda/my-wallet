673d8d10cce0efd71a15a13658c7c15105301d76
everton.lacerda <everton.lacerda@sennit.com.br>
2021-03-24 19:51:41 -0300

    fix(content):style scrollBar, fixed sidebar/heade

--- src/components/Content/style.ts
+++ src/components/Content/style.ts
@@ -6,4 +6,18 @@ export const Container = styled.div`
     background-color: ${props => props.theme.colors.primary};
 
     padding: 25px;
+
+    height: calc(100vh - 70px);
+    overflow-y: scroll;
+
+    ::-webkit-scrollbar {
+        width: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: ${props => props.theme.colors.secondary};
+    }
+    ::-webkit-scrollbar-thumb {
+        background-color: ${props => props.theme.colors.tertiary}; 
+    }
 `;
