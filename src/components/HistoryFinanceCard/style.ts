9e21ea0dd88d7127eea5a3690e31adbe431da98f
everton.lacerda <everton.lacerda@sennit.com.br>
2021-03-26 13:14:05 -0300

    feat(list):  getData list

--- src/components/HistoryFinanceCard/style.ts
+++ src/components/HistoryFinanceCard/style.ts
@@ -8,7 +8,7 @@ interface ITagProps {
 export const Container = styled.li` 
     background-color: ${props => props.theme.colors.tertiary};
     list-style: none; 
-    border-radius: 5px;
+    border-radius: 10px;
     margin:10px 0;
     padding: 12px 10px;
 
@@ -31,6 +31,11 @@ export const Container = styled.li`
         justify-content: space-between;
 
         padding-left: 10px;
+    }
+
+    > div span {
+        font-size: 22px;
+        font-weight: 500;
     }
 
 `;
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,28 +1,81 @@
-import React from 'react';
+import React, {useMemo, useState, useEffect} from 'react';
+import { match } from 'react-router';
 
 import ContentHeader from '../../components/ContentHeader';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 import SelectInput from '../../components/Selectinput';
+import expenses from '../../repositories/expenses';
+import gains from '../../repositories/gains';
 
 import { Container, Content, Filters } from './style';
+interface IRouteParams {
+    match: {
+        params: {
+            type: string;
+        }
+    }
+}
 
-const months = [
-    {value: '1', label: 'Janeiro'},
-    {value: '2', label: 'Fevereiro'},
-    {value: '3', label: 'Março'},
-]
+interface IData {
+    id: string;
+    description: string;
+    amountFormatted: string;
+    frequency: string;
+    dataFormatted: string;
+    tagColor: string;
+}
+const List: React.FC<IRouteParams> = ({ match }) => {
 
-const years = [
-    {value: '2021', label: '2021'},
-    {value: '2020', label: '2020'},
-    {value: '2019', label: '2019'},
-]
+    const [data, setData] = useState<IData[]>([]);
+    
+    const { type } = match.params;
 
+    const header = useMemo(() => {
+        return type === 
+        'entry-balance' ? {
+            title: 'Entradas',
+            lineColor: '#F7931B'
+        } : {
+            title:'Saídas',
+            lineColor: '#E44C4E'
+        } 
+    },[type]);
 
-const List: React.FC = () => {
+    const listData = useMemo(()=> {
+        return type === 'entry-balance' ? gains : expenses;
+    },[type])
+
+    useEffect(() => {
+
+        const res = listData.map((item) => {
+            return {
+                id: String(Math.random() * data.length),
+                description: item.description,
+                amountFormatted: item.amount,
+                frequency: item.frequency,
+                dataFormatted: item.date,
+                tagColor: item.frequency === 'recorrente' ? '#4E41f0' :'#E44C4E'
+            }
+        })
+
+        setData(res) 
+    },[])
+
+    const months = [
+        {value: '1', label: 'Janeiro'},
+        {value: '2', label: 'Fevereiro'},
+        {value: '3', label: 'Março'},
+    ]
+
+    const years = [
+        {value: '2021', label: '2021'},
+        {value: '2020', label: '2020'},
+        {value: '2019', label: '2019'},
+    ]
+
     return (
         <Container>
-            <ContentHeader title="Saídas" lineColer="#E44C4E">
+            <ContentHeader title={header.title} lineColer={header.lineColor}>
                 <SelectInput options={months}  />
                 <SelectInput options={years}  />
             </ContentHeader>
@@ -43,13 +96,17 @@ const List: React.FC = () => {
             </Filters>
 
             <Content>
-                <HistoryFinanceCard 
-                    tagColor="#E44C4E"
-                    title="Conta de internet"
-                    subTitle="24/03/2021"
-                    amount="R$ 130.00"
-                /> 
-
+                {
+                    data.map(item => (
+                        <HistoryFinanceCard 
+                            key={item.id}
+                            tagColor={item.tagColor}
+                            title={item.description}
+                            subTitle={item.dataFormatted}
+                            amount={item.amountFormatted}
+                        />
+                    ))
+                }
             </Content>
         </Container>
     );
